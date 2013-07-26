f6bef793c7b2a118d11ed692fe34d41558546c74
Matt Johnson <mjohn23@gmail.com>
2013-07-26 22:22:05 +0000

    Ex 1 extra credit

--- ex1.c
+++ ex1.c
@@ -3,6 +3,12 @@
 int main(int argc, char *agv[])
 {
   puts("Hello world.");
-  
+
+  // EC: Print out 5 more lines of text or something more complex than hello world.
+  puts("Hello again.");
+  puts("What's shaking?");
+  puts("This is the fourth line.");
+  puts("Annnnnnnd we're finished.");
+
   return 0;
 }
